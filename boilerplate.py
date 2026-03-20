4d92a49184e682b695306b0d07e49ddae75c246f
AMAN KUMAR <aman@atyro.atyro>
2026-03-20 13:04:04 +0530

    ucb1

--- boilerplate.py
+++ boilerplate.py
@@ -85,19 +85,32 @@ def epsilon_greedy(bandit, T, epsilon=0.1):
 
     return np.array(rewards), np.array(regrets)
 
-# # UCB1
-# def ucb1(bandit, T):
-#     K = bandit.K
-#     #Initialize algorithm dependent variable here
-#     rewards, regrets = [], []
-#     cumulative_regret = 0
+# UCB1
+def ucb1(bandit, T):
+    K = bandit.K
+    #Initialize algorithm dependent variable here
+    rewards, regrets = [], []
+    empirical_means = [] #empirical means of arms
+    no_of_pulls_per_arm = [] #no of pulls made by each arm
+    total_arm_rewards=[] #total rewards of each arm
+    cumulative_regret = 0
 
-#     # Pull each arm once
-#     for arm in range(K):
+    # Pull each arm once
+    for arm in range(K):
+        rewards.append(bandit.pull(arm))
+        empirical_means.append(rewards[arm])
+        no_of_pulls_per_arm.append(1)
+    total_arm_rewards = rewards
+    for t in range(K, T):
+        opt_arm = np.argmax(empirical_means)
+        no_of_pulls_per_arm[opt_arm]+=1;
+        rewards.append(bandit.pull(opt_arm))
+        total_arm_rewards[opt_arm] += rewards[-1]
+        empirical_means[opt_arm] = total_arm_rewards[opt_arm]/no_of_pulls_per_arm[opt_arm] + (np.sqrt((2*np.log(t+K))/no_of_pulls_per_arm[opt_arm]))
+        regrets.append(abs(bandit.best_mean-empirical_means[opt_arm])*(no_of_pulls_per_arm[opt_arm]/t))
+        cumulative_regret += regrets[-1]
 
-#     for t in range(K, T):
-
-#     return np.array(rewards), np.array(regrets)
+    return np.array(rewards), np.array(regrets)
 
 
 # Thompson Sampling
@@ -141,13 +154,12 @@ def run_experiment(means, T, n_runs):
     algorithms = {
         "Greedy":            greedy,
         "Eps-Greedy(0.1)":   lambda b, T: epsilon_greedy(b, T, epsilon=0.1),
-        # "UCB1":              ucb1,
+        "UCB1":              ucb1,
         # "KL-UCB":            kl_ucb,
         "Thompson Sampling": thompson_sampling,
     }
     results = {name: [] for name in algorithms}
 
-    X=[]
     for _ in range(n_runs):
         bandit = BernoulliBandit(means)
         X=np.arange(bandit.K,T)
